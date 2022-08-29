dfee0f3f3d817fb8defadab15a1965a6ffd574af
Delt <78700987+Deltkastel@users.noreply.github.com>
2022-08-29 16:18:03 -0600

    Update init.cmd

--- init.cmd
+++ init.cmd
@@ -3,7 +3,7 @@ Title Portable Python 3.10.6
 SET PATH=%cd%\VapourSynth;%cd%\VapourSynth\Scripts;%PATH%
 SET PYTHONPATH=%cd%\VapourSynth;%cd%\VapourSynth\Scripts;%cd%\VapourSynth\Lib\site-packages
 
-python -c "import sys; print('Python' + sys.version)"
+::python -c "import sys; print('Python' + sys.version)"
 
 doskey pip=python -m pip $*
 doskey py=python $*
