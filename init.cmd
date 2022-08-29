1e66848dc05c84b1667ff098fcf61ac351414bb9
Delt <78700987+Deltkastel@users.noreply.github.com>
2022-08-29 15:09:21 -0600

    Add files via upload

---
+++ init.cmd
@@ -0,0 +1,13 @@
+@echo off
+Title Portable Python 3.10.6
+SET PATH=%cd%\VapourSynth;%PATH%
+SET PYTHONPATH=%cd%\VapourSynth;%cd%\VapourSynth\Scripts;%cd%\VapourSynth\Lib\site-packages
+
+python -c "import sys; print('Python' + sys.version)"
+
+doskey pip=python -m pip $*
+doskey py=python $*
+doskey vspreview=python -m vspreview $*
+
+::echo PATH = %path%
+::echo PYTHONPATH = %PYTHONPATH%
